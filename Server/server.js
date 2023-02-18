28433e83bec90fcb48756ced688791cf369e1861
MicheleTornetta <112029252+MicheleTornetta@users.noreply.github.com>
2023-02-18 13:02:13 -0500

    server chat service - still need front end

--- Server/server.js
+++ Server/server.js
@@ -34,4 +34,18 @@ const server = new ApolloServer({ typeDefs, resolvers, context: authMiddleware }
       console.log(`🌍 Now listening on http://localhost:${PORT}`)
     );
   });
+
+  // Socket.io
+  const http = require('http').Server(app);
+  const io = require('socket.io')(http);
+  io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+      console.log('User Disconnected');
+    });
+    socket.on('example_message', function(msg){
+      console.log('message: ' + msg);
+    });
+  });
+  io.listen(8000);
 })();
