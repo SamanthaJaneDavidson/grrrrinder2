c635ff27ecda6ced8dc1788b87d364d9127ac131
CassNoisette <cassandrebeauzil@gmail.com>
2023-02-19 16:51:50 -0500

    fixed bg img path

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import backgoundImg from '../../../assets/img/home_bg.jpg';
+import backgoundImg from '../../../Images/bg-img.jpg';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
