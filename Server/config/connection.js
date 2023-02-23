4fa21da31b831e2e27d8351c970dfc556df4d0c4
unknown <samanthajanedavidson@gmail.com>
2023-02-23 12:58:35 -0500

    updated URL in connection.js file. moved env file to server folder

--- Server/config/connection.js
+++ Server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://${process.env.db_username}:${process.env.db_password}@grrrrinder2.argev.mongodb.net/`,
+mongoose.connect(`mongodb+srv://${process.env.db_username}:${process.env.db_password}@grrrrinder2.argev.mongodb.net/?retryWrites=true&w=majority`,
 {
   useNewUrlParser: true,
   useUnifiedTopology: true,
