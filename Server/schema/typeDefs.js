1e8a9646a03a19821c30d546b18e84b237590a9f
AmyLipscomb <amy.a.lipscomb@gmail.com>
2023-02-24 08:19:29 -0500

    added code to typedef, resolvers, queries and mutations for Stripe

--- Server/schema/typeDefs.js
+++ Server/schema/typeDefs.js
@@ -10,6 +10,7 @@ type User {
   username: String
   email: String
   saved_dogs: [Dog]!
+  orders: [Order]
 }
 
 
@@ -31,8 +32,30 @@ type Dog {
     preferred_location: [String]
 }
 
+type Category {
+  _id: ID
+  name: String
+}
 
+type Product {
+  _id: ID
+  name: String
+  description: String
+  image: String
+  quantity: Int
+  price: Float
+  category: Category
+}
 
+type Order {
+  _id: ID
+  purchaseDate: String
+  products: [Product]
+}
+
+type Checkout {
+  session: ID
+}
 
 type Auth {
   token: String
@@ -59,13 +82,21 @@ input SaveDogInput {
 type Query {
   me: User
   dog: Dog
+  categories: [Category]
+  products(category: ID, name: String): [Product]
+  product(_id: ID!): Product
+  order(_id: ID!): Order
+  checkout(products: [ID]!): Checkout
+
 }
 
 type Mutation {
   addUser(username: String!, email: String!, password: String!): Auth
+  addOrder(products: [ID]!): Order
   login(username: String!, password: String!): Auth
   saveDog(input: SaveDogInput): User
   removeDog (_id: String!): User
+  updateProduct(_id: ID!, quantity: Int!): Product
 }`;
 
 module.exports = typeDefs;
