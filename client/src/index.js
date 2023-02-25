f886669dcc82e938fc64b539c584299dfd6bf603
MicheleTornetta <112029252+MicheleTornetta@users.noreply.github.com>
2023-02-25 13:25:26 -0500

    button color

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; // may need to delete the /client if error returns  
+import ReactDOM from 'react-dom/client'; // may need to delete the /client if error returns  
 import './index.css';
 import './App.css';
 import App from './App';
