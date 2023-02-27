415680adb40f2a78e10d07ff776d534ed328cbcc
MicheleTornetta <112029252+MicheleTornetta@users.noreply.github.com>
2023-02-26 23:23:28 -0500

    Added dog card

---
+++ client/src/components/Dog.js
@@ -0,0 +1,57 @@
+import React from "react";
+// import React from 'react';
+import { Button, Card, Accordion } from "react-bootstrap";
+
+export default function Dog({ dog, saveDog }) {
+  return (
+    <Card>
+      <Card.Img
+        variant="top"
+        src={dog.dog_picture}
+        style={{ width: "100%", height: "180px", objectFit: "cover" }}
+        alt={dog.dog_name}
+      />
+
+      <Card.Body>
+        <Card.Title>{dog.dog_name}</Card.Title>
+        <Accordion>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>Information</Accordion.Header>
+            <Accordion.Body>
+              <p>breed = {dog.dog_breed}</p>
+              <p>gender = {dog.dog_gender}</p>
+              <p>size = {dog.dog_size}</p>
+              <p>age = {dog.dog_age}</p>
+              <p>vaccinations = {dog.dog_vaccinations ? "Yes" : "No"}</p>
+              <p>neuter_spayed = {dog.dog_neuter_spayed ? "Yes" : "No"}</p>
+              <p>temperment = {dog.dog_temperment}</p>
+              <p>notes = {dog.dog_notes}</p>
+            </Accordion.Body>
+          </Accordion.Item>
+
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>Avaibility</Accordion.Header>
+            <Accordion.Body>
+              <p>preferred_days = {dog.preferred_days.join(", ")}</p>
+              <p>preferred_timeofday = {dog.preferred_timeofday.join(", ")}</p>
+              <p>preferred_location = {dog.preferred_location.join(", ")}</p>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+        {saveDog ? (
+          <Button
+            onClick={() => {
+              saveDog({
+                variables: {
+                  dog_id: dog._id,
+                },
+              });
+            }}
+          >
+            Save Dog
+          </Button>
+        ) : undefined}
+      </Card.Body>
+    </Card>
+  );
+}
--- client/src/pages/User-Profile.js
+++ client/src/pages/User-Profile.js
@@ -1,10 +1,9 @@
-// import React, { useState } from 'react';
 import { useMutation, useQuery } from "@apollo/client";
 import { QUERY_ME, ADD_DOG } from "../utils/queries";
-
 // import CardColumns from 'react-bootstrap/CardColumns'
-import { Accordion, Card, Container } from "react-bootstrap";
+import { Accordion, Card, Container, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { REMOVE_DOG } from "../utils/mutations";
 
 export default function Profile() {
   const { loading, data } = useQuery(QUERY_ME);
@@ -21,10 +20,11 @@ export default function Profile() {
           preferred_timeofday: ["Morning"],
           preferred_location: ["12345"],
           preferred_days: ["Monday"],
-          dog_vaccinations: false,
+          dog_vaccinations: true,
           dog_temperment: "shy",
           dog_size: "small",
-          dog_picture: "https://res.cloudinary.com/dh82x6mau/image/upload/v1671139410/yv5ndyqoihfub2stesjy",
+          dog_picture:
+            "https://res.cloudinary.com/dh82x6mau/image/upload/v1671139410/yv5ndyqoihfub2stesjy",
           dog_notes: null,
           dog_neuter_spayed: false,
           dog_name: "dan",
@@ -40,55 +40,96 @@ export default function Profile() {
   if (loading) {
     return <h2>loading</h2>;
   }
-  // Auth.login(data.addUser.token);
+
   return (
     <>
       <Link id="add-dog-link" to="/add-dog">
         Add a Dog to Your Profile
       </Link>
-
       <Container>
-        <Accordion>
-          <Card style={{ width: "18rem" }}>
+        <Col xs={5}>
+          <Accordion>
+            <Card>
+              <Card.Img
+                variant="top"
+                src={userData.dog_picture}
+                style={{ width: "100%", height: "35vw", objectFit: "cover" }}
+                alt={userData.dog_name}
+              />
+
+              <Card.Body>
+                <Card.Title>{userData.dog_name}</Card.Title>
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>Profile</Accordion.Header>
+                  <Accordion.Body>
+                    breed = {`Breed: ${userData.dog_breed}`}
+                    gender = {`Gender: ${userData.dog_gender}`}
+                    size = {` Size: ${userData.dog_size}`}
+                    age = {` Age: ${userData.dog_age}`}
+                    vaccinations = {` Vaccinated: ${userData.dog_vaccinations}`}
+                    neuter_spayed ={" "}
+                    {` Neutered/Spayed: ${userData.dog_neuter_spayed}`}
+                    temperment ={` Temperment: ${userData.dog_temperment}`}
+                    notes = {` Additional Notes: ${userData.dog_notes}`}
+                  </Accordion.Body>
+                </Accordion.Item>
+
+                <Accordion.Item eventKey="1">
+                  <Accordion.Header>Avaibility</Accordion.Header>
+                  <Accordion.Body>
+                    preferred_days ={" "}
+                    {` Preferred Days: ${userData.preferred_days}`}
+                    preferred_timeofday ={" "}
+                    {` Preferred Times: ${userData.preferred_timeofday}`}
+                    preferred_location ={" "}
+                    {` Preferred Location: ${userData.preferred_location}`}
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Card.Body>
+            </Card>
+          </Accordion>
+            <Button onClick={addDogPress}>Add dog!</Button>
+        </Col>
+        
+        <Col xs={7}>
+          <Card className="saved-dog">
             <Card.Img
               variant="top"
               src={userData.dog_picture}
               style={{ width: "100%", height: "35vw", objectFit: "cover" }}
               alt={userData.dog_name}
             />
-
             <Card.Body>
               <Card.Title>{userData.dog_name}</Card.Title>
-              <Accordion.Item eventKey="0">
-                <Accordion.Header>Profile</Accordion.Header>
-                <Accordion.Body>
-                  breed = {`Breed: ${userData.dog_breed}`}
-                  gender = {`Gender: ${userData.dog_gender}`}
-                  size = {` Size: ${userData.dog_size}`}
-                  age = {` Age: ${userData.dog_age}`}
-                  vaccinations = {` Vaccinated: ${userData.dog_vaccinations}`}
-                  neuter_spayed ={" "}
-                  {` Neutered/Spayed: ${userData.dog_neuter_spayed}`}
-                  temperment ={` Temperment: ${userData.dog_temperment}`}
-                  notes = {` Additional Notes: ${userData.dog_notes}`}
-                </Accordion.Body>
-              </Accordion.Item>
-
-              <Accordion.Item eventKey="1">
-                <Accordion.Header>Avaibility</Accordion.Header>
-                <Accordion.Body>
-                  preferred_days ={" "}
-                  {` Preferred Days: ${userData.preferred_days}`}
-                  preferred_timeofday ={" "}
-                  {` Preferred Times: ${userData.preferred_timeofday}`}
-                  preferred_location ={" "}
-                  {` Preferred Location: ${userData.preferred_location}`}
-                </Accordion.Body>
-              </Accordion.Item>
+              <Card.Img
+                as={Image}
+                variant="top"
+                src={userData.dog_picture}
+                fluid={true}
+                alt="Dog Picture"
+              />
+              <Card.Text>
+                breed = {`Breed: ${userData.dog_breed}`}
+                gender = {`Gender: ${userData.dog_gender}`}
+                size = {` Size: ${userData.dog_size}`}
+                age = {` Age: ${userData.dog_age}`}
+                vaccinations = {` Vaccinated: ${userData.dog_vaccinations}`}
+                neuter_spayed ={" "}
+                {` Neutered/Spayed: ${userData.dog_neuter_spayed}`}
+                temperment ={` Temperment: ${userData.dog_temperment}`}
+                preferred_days = {` Preferred Days: ${userData.preferred_days}`}
+                preferred_timeofday ={" "}
+                {` Preferred Times: ${userData.preferred_timeofday}`}
+                preferred_location ={" "}
+                {` Preferred Location: ${userData.preferred_location}`}
+                notes = {` Additional Notes: ${userData.dog_notes}`}
+              </Card.Text>
+              <Button className="btn" onClick={REMOVE_DOG} variant="primary">
+                Remove Dog
+              </Button>
             </Card.Body>
           </Card>
-        </Accordion>
-        <button onClick={addDogPress}>Add dog!</button>
+        </Col>
       </Container>
     </>
   );
