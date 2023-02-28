362dc397a6ff6a5d6db6b17fd9f29c219040930f
CassNoisette <cassandrebeauzil@gmail.com>
2023-02-27 21:42:13 -0500

    updated card display

--- client/src/components/Dog.js
+++ client/src/components/Dog.js
@@ -4,11 +4,12 @@ import { Button, Card, Accordion } from "react-bootstrap";
 
 export default function Dog({ dog, saveDog, unsaveDog, deleteDog, updateState }) {
   return (
-    <Card>
-      <Card.Img
+    <Card className="card">
+      <Card.Img 
+      className = "card-img"
         variant="top"
         src={dog.dog_picture}
-        style={{ width: "100%", height: "180px", objectFit: "cover" }}
+        style={{ width: "100%", height: "18rem", objectFit: "cover" }}
         alt={dog.dog_name}
       />
 
@@ -16,25 +17,25 @@ export default function Dog({ dog, saveDog, unsaveDog, deleteDog, updateState })
         <Card.Title>{dog.dog_name}</Card.Title>
         <Accordion>
           <Accordion.Item eventKey="0">
-            <Accordion.Header>Information</Accordion.Header>
+            <Accordion.Header>About Me</Accordion.Header>
             <Accordion.Body>
-              <p>breed = {dog.dog_breed}</p>
-              <p>gender = {dog.dog_gender}</p>
-              <p>size = {dog.dog_size}</p>
-              <p>age = {dog.dog_age}</p>
-              <p>vaccinations = {dog.dog_vaccinations ? "Yes" : "No"}</p>
-              <p>neuter_spayed = {dog.dog_neuter_spayed ? "Yes" : "No"}</p>
-              <p>temperment = {dog.dog_temperment}</p>
-              <p>notes = {dog.dog_notes}</p>
+              <p>Breed: {dog.dog_breed}</p>
+              <p>Gender: {dog.dog_gender}</p>
+              <p>Size: {dog.dog_size}</p>
+              <p>Age: {dog.dog_age}</p>
+              <p>Vaccinated:{dog.dog_vaccinations ? "Yes" : "No"}</p>
+              <p>Neutered or Spayed: {dog.dog_neuter_spayed ? "Yes" : "No"}</p>
+              <p>Temperment: {dog.dog_temperment}</p>
+              <p>Additional Info: {dog.dog_notes}</p>
             </Accordion.Body>
           </Accordion.Item>
 
           <Accordion.Item eventKey="1">
             <Accordion.Header>Avaibility</Accordion.Header>
             <Accordion.Body>
-              <p>preferred_days = {dog.preferred_days.join(", ")}</p>
-              <p>preferred_timeofday = {dog.preferred_timeofday.join(", ")}</p>
-              <p>preferred_location = {dog.preferred_location.join(", ")}</p>
+              <p>Preferred Days:{dog.preferred_days.join(", ")}</p>
+              <p>Preferred Time of Day:{dog.preferred_timeofday.join(", ")}</p>
+              <p>Preferred Location:{dog.preferred_location.join(", ")}</p>
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
